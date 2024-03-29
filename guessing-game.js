91c933b941614b0e9475cc47dc730077cd0ed9cd
syed-ahmed-raza <ar03110386073@gmail.com>
2024-03-29 23:15:53 +0500

    created number guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,11 +1,12 @@
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
-console.log(randomNumber);
-const answer = await inquirer.prompt([{
+const answer = await inquirer.prompt([
+    {
         name: "guessingnumber",
         type: "number",
-        message: "Please guess a number between 1-6:"
-    }]);
+        message: "Please guess a number between 1-6:",
+    },
+]);
 if (answer.guessingnumber === randomNumber) {
     console.log("Congratulations! you guessed right number.");
 }
