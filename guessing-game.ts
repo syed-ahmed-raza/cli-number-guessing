e9094d7d8a563fe73ce90febd34122e51339e955
syed-ahmed-raza <ar03110386073@gmail.com>
2024-03-29 23:12:46 +0500

    created number guessing game

---
+++ guessing-game.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "guessingnumber",
+    type: "number",
+    message: "Please guess a number between 1-6:",
+  },
+]);
+
+if (answer.guessingnumber === randomNumber) {
+  console.log("Congratulations! you guessed right number.");
+} else {
+  console.log("You guest wrong number");
+}
